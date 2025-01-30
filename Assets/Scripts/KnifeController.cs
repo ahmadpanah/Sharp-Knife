74dec00b8d8671e2d15fb6f55ba3c3446ea599f6
zahra <habiballahi>
2025-01-30 22:30:43 +0330

    Fix DisableKnifeIconColor Problem

---
+++ Assets/Scripts/KnifeController.cs
@@ -0,0 +1,69 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KnifeController : MonoBehaviour
+{
+    private KnifeManager knifeManager;
+    private Rigidbody2D knifeRigidBody;
+    [SerializeField] private float moveSpeed;
+
+    private bool canShoot;
+
+    private void Start()
+    {
+        GetComponentValues();
+    }
+
+    private void Update()
+    {
+        HandleShootInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Shoot();
+    }
+
+    private void HandleShootInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+            canShoot = true;
+            knifeManager.SetDisableKnifeIconColor();
+
+        }
+    }
+
+    private void Shoot()
+    {
+        if (canShoot)
+        {
+            knifeRigidBody.AddForce(Vector2.up * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Circle"))
+        {
+            knifeManager.SetActiveKnife();
+            canShoot = false;
+            knifeRigidBody.isKinematic = true;
+            transform.SetParent(other.gameObject.transform);
+           
+        }
+
+        if (other.gameObject.CompareTag("Knife")){
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+    private void GetComponentValues()
+    {
+        knifeRigidBody = GetComponent<Rigidbody2D>();
+        knifeManager = GameObject.FindObjectOfType<KnifeManager>();
+    }
+}
---
+++ Assets/Scripts/KnifeManager.cs
@@ -0,0 +1,64 @@
+
+using System.Collections.Generic;
+//using UnityEditor.U2D.Path;
+using UnityEngine;
+
+public class KnifeManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> knifeList = new List<GameObject>();
+    [SerializeField] private List<GameObject> knifeIconList = new List<GameObject>();
+
+    [SerializeField] private GameObject knifePrefab;
+    [SerializeField] private GameObject knifeIconPrefab;
+
+    [SerializeField] private Vector2 knifeIconPosition;
+
+    [SerializeField] private Color activeColor;
+    [SerializeField] private Color disableColor;
+
+    [SerializeField] private int knifeCount;
+
+    private int knifeIndex = 0;
+
+    private void Start()
+    {
+        CreateKnifes();
+        CreateKnifeIcons();
+    }
+
+    private void CreateKnifes()
+    {
+        for (int i = 0; i < knifeCount; i++)
+        {
+            GameObject newKnife = Instantiate(knifePrefab, transform.position, Quaternion.identity);
+            newKnife.SetActive(false);
+            knifeList.Add(newKnife);
+        }
+        knifeList[0].SetActive(true);
+    }
+
+    private void CreateKnifeIcons()
+    {
+        for (int i = 0; i < knifeCount; i++)
+        {
+            GameObject newKnifeIcon = Instantiate(knifeIconPrefab, knifeIconPosition, knifeIconPrefab.transform.rotation);
+            newKnifeIcon.GetComponent<SpriteRenderer>().color = activeColor;
+            knifeIconPosition.y += 0.5f;
+            knifeIconList.Add(newKnifeIcon);
+        }
+    }
+
+    public void SetDisableKnifeIconColor()
+    {
+        knifeIconList[(knifeIconList.Count-1 ) - knifeIndex].GetComponent<SpriteRenderer>().color = disableColor;
+    }
+
+    public void SetActiveKnife()
+    {
+        if (knifeIndex < knifeCount-1)
+        {
+            knifeIndex++;
+            knifeList[knifeIndex].SetActive(true);
+        }
+    }
+}
